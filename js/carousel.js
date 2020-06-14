7fbee0563762b215673996fb624bba0f14d0b6f2
Clinton Lunn <cpl09c@gmail.com>
2020-06-14 16:55:22 -0400

    dyamically create carousel based on files in folder

---
+++ js/carousel.js
@@ -0,0 +1,27 @@
+$.ajax({
+    url: "./img/announcements",
+    success: function(data) {
+       $(data).find("a:contains(.jpg)").each(function(response) {
+            // console.log($(this).attr("href"));
+            // console.log(response);
+            if (response === 0) {
+                console.log({ifresponse: response});
+                // $('.carousel-indicators').prepend(`
+                // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                // `)
+                $('.carousel-inner').prepend(`
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src=".${$(this).attr("href")}">
+                </div>`)
+            } else {                
+                // $('.carousel-indicators').prepend(`
+                // <li data-target="#carouselExampleIndicators" data-slide-to="${response}"></li>
+                // `)
+                $('.carousel-inner').prepend(`
+                <div class="carousel-item">
+                    <img class="d-block w-100" src=".${$(this).attr("href")}">
+                </div>`)
+            }   
+       });
+    }
+  });
