2f3f87024929fa2913bb7f69728d7d75830d5b11
Clinton Lunn <cpl09c@gmail.com>
2020-06-15 23:22:22 -0400

    folder change

--- docs/img/announcements/carousel.js
+++ docs/img/announcements/carousel.js
@@ -1,69 +1,9 @@
 console.log('still troublshooting');
 
 $.ajax({
-    url: "./",
+    url: "../announcements/",
     success: function(data) {
-        console.log("success?");
-        console.log(data);
-       $(data).find("a:contains(.jpg)").each(function(response) {
-            console.log($(this).attr("href"));
-            console.log(response);
-            if (response === 0) {
-                console.log({ifresponse: response});
-                // $('.carousel-indicators').prepend(`
-                // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                // `)
-                $('.carousel-inner').prepend(`
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src=".${$(this).attr("href")}">
-                </div>`)
-            } else {                
-                // $('.carousel-indicators').prepend(`
-                // <li data-target="#carouselExampleIndicators" data-slide-to="${response}"></li>
-                // `)
-                $('.carousel-inner').prepend(`
-                <div class="carousel-item">
-                    <img class="d-block w-100" src=".${$(this).attr("href")}">
-                </div>`)
-            }   
-       });
-    }
-  });
-
-  $.ajax({
-    url: "../",
-    success: function(data) {
-        console.log("success");
-        console.log(data);
-       $(data).find("a:contains(.jpg)").each(function(response) {
-            console.log($(this).attr("href"));
-            console.log(response);
-            if (response === 0) {
-                console.log({ifresponse: response});
-                // $('.carousel-indicators').prepend(`
-                // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                // `)
-                $('.carousel-inner').prepend(`
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src=".${$(this).attr("href")}">
-                </div>`)
-            } else {                
-                // $('.carousel-indicators').prepend(`
-                // <li data-target="#carouselExampleIndicators" data-slide-to="${response}"></li>
-                // `)
-                $('.carousel-inner').prepend(`
-                <div class="carousel-item">
-                    <img class="d-block w-100" src=".${$(this).attr("href")}">
-                </div>`)
-            }   
-       });
-    }
-  });
-
-  $.ajax({
-    url: "../../",
-    success: function(data) {
-        console.log("success");
+        console.log("folder change");
         console.log(data);
        $(data).find("a:contains(.jpg)").each(function(response) {
             console.log($(this).attr("href"));
